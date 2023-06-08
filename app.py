9e527f77c367e5d6d44590663ffea11980a8ae20
itay <itaylev20@gmail.com>
2023-06-08 13:04:50 +0300

    Upload web

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 app = Flask(__name__)
 
 ROUTES_FILE_PATH = "data/routes.txt"
+STOPS_FILE_PATH = "data/stops.txt"
+TRIPS_FILE_PATH = "data/trips.txt"
 
 @app.route('/')
 def main():
@@ -13,7 +15,18 @@ def main():
 
 @app.route('/get_routes')
 def get_routes():
-    with open(ROUTES_FILE_PATH, "r", encoding='utf-8') as file:
+    return get_csv_data(ROUTES_FILE_PATH)
+
+@app.route('/get_trips')
+def get_trips():
+    return get_csv_data(TRIPS_FILE_PATH)
+@app.route('/get_stops')
+def get_stops():
+    return get_csv_data(STOPS_FILE_PATH)
+
+
+def get_csv_data(filename):
+    with open(filename, "r", encoding='utf-8') as file:
         csv_reader = csv.reader(file,)
         headers = next(csv_reader)
         # Create a list of dictionaries
