3254d0bfe3159bcdafafa3d9978037086f3783ad
itay <itaylev20@gmail.com>
2023-06-08 11:46:05 +0300

    Upload web

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import logging
-import azure.functions as func
 from flask import Flask, request, render_template, redirect,url_for
-import functools
 
 app = Flask(__name__)
 
@@ -13,4 +10,8 @@ def main():
     return render_template(f"main.html",bus_lines=bus_lines)
 
 
+@app.route('/get_routes')
+def get_routes():
+    bus_lines= [i for i in range(190)]
+    return render_template(f"main.html",bus_lines=bus_lines)
 
